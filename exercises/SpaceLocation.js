5f116f454279f403e8b52794e0af5e6ef64cf1f1
the8thbit <the8thbit@gmail.com>
2020-07-19 19:56:08 -0500

    complete exercise 27.7

---
+++ exercises/SpaceLocation.js
@@ -0,0 +1,12 @@
+"use strict";
+// Paste in the provided code here:
+exports.__esModule = true;
+exports.SpaceLocation = void 0;
+var SpaceLocation = /** @class */ (function () {
+    function SpaceLocation(name, kilometersAway) {
+        this.name = name;
+        this.kilometersAway = kilometersAway;
+    }
+    return SpaceLocation;
+}());
+exports.SpaceLocation = SpaceLocation;
